81742ed774f321be6d73f9affeceede1251b45a0
kiriharu <kiriharu@yandex.ru>
2020-10-01 23:02:58 +0300

    user model, user middleware, register user on /start

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from loguru import logger
-from asyncio import Queue
-
+from tortoise import Tortoise
+from middlewares.userdata import UserMiddleware
 import config
 import handlers
 
-picture_queue = Queue()
+
 storage = MemoryStorage()
 telegram_bot = Bot(token=config.TELEGRAM_BOT_TOKEN)
 dp = Dispatcher(telegram_bot, storage=storage)
@@ -16,6 +16,18 @@ def on_startup():
     logger.info("Register handlers...")
     # Register you handlers here.
     handlers.default.setup(dp)
+    dp.middleware.setup(UserMiddleware())
+
+
+async def database_init():
+    await Tortoise.init(
+        db_url='sqlite://db.sqlite3',
+        modules={
+            'model': ['models.user']
+        }
+    )
+    await Tortoise.generate_schemas()
+    logger.info("Tortoise inited!")
 
 # TODO: Добавить сохранение настроек и генерирование картинок с настроек
 # TODO: Мидлварь для логированния и сбора статистики (а че крашнулось чтобы понимать)
@@ -24,4 +36,5 @@ def on_startup():
 
 if __name__ == "__main__":
     on_startup()
+    dp.loop.create_task(database_init())
     executor.start_polling(dp, skip_updates=True)
--- handlers/default/starting.py
+++ handlers/default/starting.py
@@ -1,6 +1,9 @@
 from aiogram.types import Message
 from config import STARTING_MESSAGE
+from middlewares.userdata import userdata_required
+from models.user import User
 
 
-async def start(msg: Message):
+@userdata_required
+async def start(msg: Message, user: User):
     await msg.answer(STARTING_MESSAGE)
