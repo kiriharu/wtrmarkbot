259cc60cddc63d407d9f20cef5e6045368c80729
Crystal Melting Dot <stresspassing@gmail.com>
2020-10-04 13:11:39 +0300

    Better get_xy

--- consts.py
+++ consts.py
@@ -1,10 +1,25 @@
 import os
+from enum import IntEnum
 
 
 MAX_FONT_SIZE = 96
 MARGIN = 10
 
 FONTS = list(map(lambda x: x.replace(".ttf", ""), os.listdir("fonts")))
+
+
+class Side(IntEnum):
+    CENTER = 0
+    TOP = 1
+    BOTTOM = 4
+    LEFT = 2
+    RIGHT = 8
+
+    TOP_LEFT = TOP | LEFT
+    TOP_RIGHT = TOP | RIGHT
+    BOTTOM_LEFT = BOTTOM | LEFT
+    BOTTOM_RIGHT = BOTTOM | RIGHT
+
 
 TEXT_COLORS = {
     "Белый": [255, 255, 255],
@@ -15,9 +30,14 @@ TEXT_COLORS = {
 }
 
 POSITIONS = {
-    "Сверху в левом углу": 1,
-    "Сверху в правом углу": 2,
-    "Снизу в левом углу": 3,
-    "Снизу в правом углу": 4,
-    "По центру": 5
+    "Сверху": Side.TOP,
+    "Снизу": Side.BOTTOM,
+    "Слева": Side.LEFT,
+    "Справа": Side.RIGHT,
+
+    "Сверху в левом углу": Side.TOP_LEFT,
+    "Сверху в правом углу": Side.TOP_RIGHT,
+    "Снизу в левом углу": Side.BOTTOM_LEFT,
+    "Снизу в правом углу": Side.BOTTOM_RIGHT,
+    "По центру": Side.CENTER
 }
