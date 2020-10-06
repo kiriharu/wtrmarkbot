a9cee18ebc75f2eba83c6de022c6437c82eb5356
kiriharu <kiriharu@yandex.ru>
2020-10-06 23:05:38 +0300

    mysql support in bot

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ if os.path.exists(dotenv_path):
 
 # Loading token from .env
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# Mysql
+MYSQL_HOST = os.getenv("MYSQL_HOST", None)  # if none, use sqlite db
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_PORT = os.getenv("MYSQL_PORT", 3306)
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "wtrmarkbot")
